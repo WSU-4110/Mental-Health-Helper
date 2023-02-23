6fc493b6f2caa68b60f4da213265d495d6a41117
Amrita Dhar <104931835+yellow-tulips@users.noreply.github.com>
2023-02-22 23:53:31 -0500

    Contact form needed to be updated

--- contactform.php
+++ contactform.php
@@ -1,4 +1,3 @@
-
 <?php
   $fname = $_POST['firstname'];
   $lname = $_POST['lastname'];
@@ -14,5 +13,11 @@
   $headers .= "Reply-To: $email \r\n";
 
 
-  mail("hd3969@wayne.edu", $subject, "You have a new message! \n Name: $fname $lname \n Age Range: $age \n Email: $email \n Phone Number: $number \n Message: $feedback", $from);
+  $sent = mail("hd3969@wayne.edu", $subject, "You have a new message! \n Name: $fname $lname \n Age Range: $age \n Email: $email \n Phone Number: $number \n Message: $feedback", $from);
+
+  if ($sent) {
+    header('Location: about.html');
+    echo "We will get back to you shortly.";
+  }
+
 ?>
