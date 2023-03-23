53e1882a64234c4cb95d0f93db505e7ce8149229
farzanaisrat <88343880+farzanaisrat@users.noreply.github.com>
2023-03-22 21:02:29 -0400

    Update links.js

--- links.js
+++ links.js
@@ -1,14 +1,30 @@
-var coll = document.getElementsByClassName("collapsible");
-    var i;
+function CollapsibleFactory() {}
 
-    for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight){
-        content.style.maxHeight = null;
-        } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-        } 
-    });
-    }
+CollapsibleFactory.prototype.createCollapsible = function(element) {
+  return new Collapsible(element);
+};
+
+function Collapsible(element) {
+  this.element = element;
+  this.element.addEventListener("click", this.toggle.bind(this));
+}
+
+Collapsible.prototype.toggle = function() {
+  this.element.classList.toggle("active");
+  var content = this.element.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+};
+
+var coll = document.getElementsByClassName("collapsible");
+var collapsibleFactory = new CollapsibleFactory();
+var collapsibles = [];
+
+for (var i = 0; i < coll.length; i++) {
+  var collapsible = collapsibleFactory.createCollapsible(coll[i]);
+  collapsibles.push(collapsible);
+}
+
